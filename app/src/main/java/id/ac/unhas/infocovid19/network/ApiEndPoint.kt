f9df5656d2ccb909bfd52f9b4bb710c10c144594
WilliamWijaya <williamwijaya3540@gmail.com>
2020-04-12 17:18:00 +0800

    RecyclerView dan Retrofit MVC (provinsi).

--- app/src/main/java/id/ac/unhas/infocovid19/network/ApiEndPoint.kt
+++ app/src/main/java/id/ac/unhas/infocovid19/network/ApiEndPoint.kt
@@ -1,5 +1,6 @@
 package id.ac.unhas.infocovid19.network
 
+import id.ac.unhas.infocovid19.model.DataHarian
 import id.ac.unhas.infocovid19.model.DataPerkasus
 import id.ac.unhas.infocovid19.model.DataProvinsi
 import retrofit2.Call
@@ -10,4 +11,6 @@ interface ApiEndPoint {
     fun getDataProvinsi() : Call<DataProvinsi>
     @GET("kasus")
     fun getDataPerkasus() : Call<DataPerkasus>
+    @GET("harian")
+    fun getDataHarian() : Call<DataHarian>
 }
